8690f0b184c73b6c959edbf2f56fb7dd54041fdd
Chris Shim <94628616+ChrisShim98@users.noreply.github.com>
2023-01-10 21:25:06 -0500

    Added about me page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { UsersPageComponent } from './users-page/users-page.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'users', component: UsersPageComponent},
+  {path: 'about-me', component: AboutMeComponent}
 ];
 
 @NgModule({
